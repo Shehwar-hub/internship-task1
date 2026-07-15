f3500a8c3fbe6fcb4804461d1bfffd513861748b
DUR E SHEHWAR <dureshehwar1503@gmail.com>
2026-07-15 14:52:48 +0500

    Add files via upload

---
+++ TASK1.py
@@ -0,0 +1,23 @@
+import random
+
+while True:
+    print("WELCOME TO ROCK, PAPER, SCISSOR GAME!!!")
+    move = input("What's your move (rock,paper,scissor)? Type'end' to quit: ".lower())
+    if move == "end":
+        print("Quiting the game.\nThank You!")
+        break
+    elif move in ["rock","paper","scissors"]:
+        comp_move = random.choice(["rock","paper","scissors"])
+        print("Computer move: ",comp_move.capitalize())
+        print("Your move:",move.capitalize())
+        if move == comp_move:
+            print("It's a TIE")
+        elif (move == "rock" and comp_move == "scissors") or \
+            (move == "scissors" and comp_move == "paper") or \
+            (move == "paper" and comp_move == "rock"):
+            print("Congratulations!! You Won")
+        else:
+            print("Computer Won")
+
+    else:
+        print("Invalid move...")
